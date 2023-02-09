5c7c7cfe12172d9ddabae3e5bff41fcbb7d8a568
Alessia Cecere <alessia.cecere@studenti.unimi.it>
2023-02-09 14:11:05 +0000

    Parallelize gauss script and show progressbar

--- mix_gauss.py
+++ mix_gauss.py
@@ -3,12 +3,14 @@ import os
 import pymc3 as pm
 import arviz as az
 from theano import tensor as T
-from sklearn.metrics import mean_squared_error
 import numpy as np
 from my_utils import modeler
 import importlib
 importlib.reload(modeler)
 import pickle
+from fastprogress import fastprogress
+fastprogress.printing = lambda: True
+from multiprocessing import cpu_count
 
 n_components = 2
 
@@ -17,7 +19,6 @@ AGGREGATED_PATH = "output/aggregated_features/"
 quest_before = pd.read_csv("datasets/EyeT/Questionnaire_datasetIA.csv")
 quest_before.index.name = "Participant"
 free_viewing_empathy = quest_before[quest_before.index%2 == 0]["Total Score original"]
-free_viewing_empathy.hist();
 
 free_fix_features_train, free_fix_labels_train, free_sac_features_train, free_sac_labels_train =  modeler.get_features(AGGREGATED_PATH, "train", "free")
 
@@ -43,7 +44,7 @@ with pm.Model() as free_fix_empathy:
 
     likelihood = pm.NormalMixture('likelihood', weights, μ,  σ, observed=y, shape=X.eval().shape[0])
 
-    free_fix_empathy_trace = pm.sample(4000, tune=2000)
+    free_fix_empathy_trace = pm.sample(cores = cpu_count)
 
 with open("models/free_fix_gaussian_mixture", 'wb') as buff:
     pickle.dump({'model': free_fix_empathy, 'trace': free_fix_empathy_trace}, buff)
